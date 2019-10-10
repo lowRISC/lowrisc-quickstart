928a73f3624cc682f4ceb7e92aeb39706bd947b8
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2019-10-10 08:49:29 +0100

    Add cfgmem programming

--- cardmem.sh
+++ cardmem.sh
@@ -4,9 +4,9 @@ echo ' # partition table of image\' \
      'label-id: 0xdeadbeef\' \
      'unit: sectors\' \
      '\' \
-     '   image1 : start=     2048, size=    65535, Id= c, bootable\' \
-     '   image2 : start=    67584, size=  4194304, Id=83\' \
-     '   image3 : start=  4261888, size=  1048576, Id=82\' \
-     '   image4 : start=  5310464, size=  1048576, Id=83\' \
-    | tr '\\' '\012' | sed 's=^\ ==' | /sbin/sfdisk -f $*
-echo ", +" | /sbin/sfdisk -f -N 4 $*
+     '    2048      65535   c  *\' \
+     '   67584    4194304   L  -\' \
+     '   4261888  1048576   S  -\' \
+     '   5310464        +   L  -\' \
+    | tr '\\' '\012' | sed 's=^\ ==' | tee sfdisk.log | /sbin/sfdisk -f $*
+sleep 2
